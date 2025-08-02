1ad11ec99d0e6ffaf28866c04050a38951edd765
DylanMckenzie <dylan20061@outlook.com>
2025-08-02 03:04:02 +0100

    deplo

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
